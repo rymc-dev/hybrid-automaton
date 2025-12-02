19ef36439230c8e8941f36801373be8383253914
Ryan McKee <ryanmckee47@icloud.com>
2025-12-02 17:51:31 +0000

    added hybrid automaton runtime and evaluation to the hybrid_automaton pkg

---
+++ src/hybrid_automaton_runner/__init__.py
@@ -0,0 +1,5 @@
+from .runner import runner
+
+__all__ = [
+    "runner"
+]
--- src/hybrid_automaton_runner/runner.py
+++ src/hybrid_automaton_runner/runner.py
@@ -1,4 +1,7 @@
 import asyncio
 
+# TODO: Move asyncio runner here
+#       for the ha runtime.
+
 async def runner(**kwargs):
-     
+     ...
