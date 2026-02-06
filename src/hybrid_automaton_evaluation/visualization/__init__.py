601230ac4b494807fef491011144606bdf5db6f0
Ryan McKee <ryanmckee47@icloud.com>
2026-02-06 12:26:25 +0000

    updates

--- src/hybrid_automaton_evaluation/visualization/__init__.py
+++ src/hybrid_automaton_evaluation/visualization/__init__.py
@@ -2,7 +2,11 @@ from .figure_generator import (
     continuous_states_over_time_fig,
     auxiliary_states_over_time_fig,
     control_inputs_over_time_fig,
-    automaton_states_over_time
+    mode_timeline_fig,
+    mode_duration_pie_chart_fig,
+    mode_visit_count_bar_chart_fig,
+    transition_matrix_heatmap_fig,
+    chattering_detection_fig
 )
 
 __author__ = "Ryan Mckee"
@@ -12,5 +16,10 @@ __all__ = [
     'continuous_states_over_time_fig',
     'auxiliary_states_over_time_fig',
     'control_inputs_over_time_fig',
-    'automaton_states_over_time'
+    'automaton_states_over_time',
+    'mode_timeline_fig',
+    'mode_duration_pie_chart_fig',
+    'mode_visit_count_bar_chart_fig',
+    'transition_matrix_heatmap_fig',
+    'chattering_detection_fig'
 ]
